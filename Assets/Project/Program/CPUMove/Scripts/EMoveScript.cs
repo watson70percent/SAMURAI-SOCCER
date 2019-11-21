f15bbf41f50f9e95c374f35052c6723551dea160
watson70percent <38928314+watson70percent@users.noreply.github.com>
2019-11-21 17:52:32 +0900

    ちょっと作りかけてる

---
+++ Assets/Project/Program/CPUMove/Scripts/EMoveScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EMoveScript : MonoBehaviour
+{
+    public int[] field = { 80, 100, 80 };
+
+    enum moveDirection
+    {
+        NE,
+        E,
+        SE,
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
