5c5bf8562b4a97e2717029d1e5babf1a92f6e807
kumachan0210 <kumachan0220ios@gmail.com>
2019-12-06 20:43:23 +0900

    ボール持っている人の（仮）

--- Assets/Project/Program/AITest/Scripts/AIBase.cs
+++ Assets/Project/Program/AITest/Scripts/AIBase.cs
@@ -4,12 +4,35 @@ using UnityEngine;
 
 public class AIBase
 {
-    protected int[,] valueMap = new int[60, 100];
-    protected bool ally;
-    protected GameObject[] teamMate = new GameObject[10];
-    protected GameObject[] opponent = new GameObject[10];
+    protected int[,] benefitMap = new int[60, 100];
+    protected int[,] riskMap = new int[60, 100];
+    protected int goal;
+    public GameObject[] teamMate = new GameObject[10];
+    public GameObject[] opponent = new GameObject[10];
+    public BallControler ball;
 
     public virtual void Revaluation() { }
 
-    public virtual Vector2 MaxValuePoint(Vector2 self) { return self; }
+    public virtual Vector2 MaxValuePoint(StrategyMode strategy = StrategyMode.Nomal) { return Vector2.zero; }
+
+    protected float CalcDistance(int point1_x,int point1_y,int point2_x, int point2_y)
+    {
+        float temp = 10 - Mathf.Abs(point1_x - point2_x) - Mathf.Abs(point1_y - point2_y);
+        if(temp < 0)
+        {
+            temp = 0;
+        }
+        temp += 1;
+
+        temp = Mathf.Log10(temp);
+
+        return temp;
+    }
+}
+
+public enum StrategyMode
+{
+    Nomal,
+    Positive,
+    Passive
 }
---
+++ Assets/Project/Program/AITest/Scripts/BallHolderTM.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallHolderTM : AIBase
+{
+    public override void Revaluation()
+    {
+        CalcPositive();
+        CalcRiskMap();
+        AddDribbleBenefit(ball.owner);
+    }
+
+    private void CalcPositive()
+    {
+        foreach (var mate in teamMate)
+        {
+            int x = (int)mate.transform.position.x;
+            int y = (int)mate.transform.position.y;
+
+            int x_min = (x - 10) > 0 ? x - 10 : 0;
+            int y_min = (y - 10) > 0 ? y - 10 : 0;
+            int x_max = (x + 10) < 60 ? x + 10 : 60;
+            int y_max = (y + 10) < 100 ? y + 10 : 100;
+
+            int to_goal = Mathf.Abs(30 - x) + Mathf.Abs(100 - y);
+
+            for (int i = x_min; i < x_max; i++)
+            {
+                for (int j = y_min; j < y_max; j++)
+                {
+                    benefitMap[i, j] = (int)(to_goal * CalcDistance(i, j, x, y));
+                }
+            }
+        }
+    }
+
+    private void CalcRiskMap()
+    {
+        foreach (var mate in opponent)
+        {
+            int x = (int)mate.transform.position.x;
+            int y = (int)mate.transform.position.y;
+
+            int x_min = (x - 10) > 0 ? x - 10 : 0;
+            int y_min = (y - 10) > 0 ? y - 10 : 0;
+            int x_max = (x + 10) < 60 ? x + 10 : 60;
+            int y_max = (y + 10) < 100 ? y + 10 : 100;
+
+            int to_goal = Mathf.Abs(30 - x) + Mathf.Abs(y);
+
+            for (int i = x_min; i < x_max; i++)
+            {
+                for (int j = y_min; j < y_max; j++)
+                {
+                    riskMap[i, j] = (int)(to_goal * CalcDistance(i, j, x, y));
+                }
+            }
+        }
+    }
+
+
+    private void AddDribbleBenefit(GameObject mate)
+    {
+        int x = (int)mate.transform.position.x;
+        int y = (int)mate.transform.position.y;
+
+        int x_min = (x - 10) > 0 ? x - 10 : 0;
+        int y_min = (y - 10) > 0 ? y - 10 : 0;
+        int x_max = (x + 10) < 60 ? x + 10 : 60;
+        int y_max = (y + 10) < 100 ? y + 10 : 100;
+
+        for (int i = x_min; i < x_max; i++)
+        {
+            for (int j = y_min; j < y_max; j++)
+            {
+                if (CalcDistance(i, j, x, y) > 0)
+                {
+                    benefitMap[i, j] += 10;
+                }
+            }
+        }
+    }
+
+    public override Vector2 MaxValuePoint(StrategyMode strategy = StrategyMode.Nomal)
+    {
+        Vector2 temp = new Vector2();
+        int max = -1000;
+        for(int i = 0; i < 60; i++)
+        {
+            for(int j = 0; j < 100; j++)
+            {
+                switch (strategy)
+                {
+                    case StrategyMode.Nomal:
+                        if(max < benefitMap[i,j] - riskMap[i, j])
+                        {
+                            max = benefitMap[i, j] - riskMap[i, j];
+                            temp = new Vector2(i, j);
+                        }break;
+
+                    case StrategyMode.Positive:
+                        if (max < benefitMap[i, j] * 2 - riskMap[i, j])
+                        {
+                            max = benefitMap[i, j] * 2 - riskMap[i, j];
+                            temp = new Vector2(i, j);
+                        }
+                        break;
+
+                    case StrategyMode.Passive:
+                        if (max < benefitMap[i, j] - riskMap[i, j] * 2)
+                        {
+                            max = benefitMap[i, j] - riskMap[i, j] * 2;
+                            temp = new Vector2(i, j);
+                        }
+                        break;
+                }
+            }
+        }
+        return temp;
+    }
+}
