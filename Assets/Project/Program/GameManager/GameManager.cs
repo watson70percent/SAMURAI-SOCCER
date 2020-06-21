426bb6ff3cc9b11d359cfa093238ea37512e0bec
DESKTOP-APK21IT\itoka229 <itoka229@yahoo.co.jp>
2020-06-21 18:40:26 +0900

    IceMilkTeaの追加、MainGameに必要なStateMachieと各Stateの追加

---
+++ Assets/Project/Program/GameManager/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IceMilkTea.Core;
+
+public class GameManager : MonoBehaviour
+{
+    private enum GameStateId
+    {
+        AllReseted,
+        Play,
+        Pose,
+        PoseBack,
+        PlayAgain,
+        Finish,
+    }
+    //このGameManagerクラスのステートマシーン
+    private ImtStateMachine<GameManager> _gameManagerStateMachine;
+    private void Awake()
+    {
+        _gameManagerStateMachine = new ImtStateMachine<GameManager>(this);
+        _gameManagerStateMachine.AddTransition<ResetState,StandbyState>((int)GameStateId.AllReseted);
+        _gameManagerStateMachine.AddTransition<StandbyState,PlayingState>((int)GameStateId.Play);
+        _gameManagerStateMachine.AddTransition<PlayingState, PoseState>((int)GameStateId.Pose);
+        _gameManagerStateMachine.AddTransition<PoseState, PlayingState>((int)GameStateId.PoseBack);
+        _gameManagerStateMachine.AddTransition<PoseState, ResetState>((int)GameStateId.PlayAgain);
+        _gameManagerStateMachine.AddTransition<PlayingState,FinishState>((int)GameStateId.Finish);
+
+        _gameManagerStateMachine.SetStartState<ResetState>();
+    }
+    private void Start()
+    {
+        //ステートマシンを起動
+        _gameManagerStateMachine.Update();
+    }
+    private void Update()
+    {
+        //ステートマシンを更新
+        _gameManagerStateMachine.Update();
+    }
+
+    public void AllResetedSignal()
+    {
+        //ステートマシンにAllResetedを送る
+        _gameManagerStateMachine.SendEvent((int)GameStateId.AllReseted);
+    }
+
+    public void Playsignal()
+    {
+        //ステートマシンにPlayを送る
+        _gameManagerStateMachine.SendEvent((int)GameStateId.Play);
+    }
+
+    public void PoseSignal()
+    {
+        //ステートマシンにPoseを送る
+        _gameManagerStateMachine.SendEvent((int)GameStateId.Pose);
+    }
+
+    public void PoseBackSignal()
+    {
+        //ステートマシンにBackを送る
+        _gameManagerStateMachine.SendEvent((int)GameStateId.PoseBack);
+    }
+
+    public void PlayAgainSignal()
+    {
+        //ステートマシンにPlayAgainを送る
+        _gameManagerStateMachine.SendEvent((int)GameStateId.PlayAgain);
+    }
+
+    public void FinishSignal()
+    {
+        //ステートマシンにFinishを送る
+        _gameManagerStateMachine.SendEvent((int)GameStateId.Finish);
+    }
+
+
+
+}
---
+++ Assets/Project/Program/GameManager/ResetState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IceMilkTea.Core;
+
+public class ResetState : ImtStateMachine<GameManager>.State
+{
+    private bool flag;
+    public bool Flag
+    {
+        get {return flag; }
+        private set {flag = value; }
+    }
+    // 状態へ突入時の処理はこのEnterで行う
+    protected override void Enter()
+    {
+    }
+
+    // 状態の更新はこのUpdateで行う
+    protected override void Update()
+    {
+    }
+
+    // 状態から脱出する時の処理はこのExitで行う
+    protected override void Exit()
+    {
+    }
+}
