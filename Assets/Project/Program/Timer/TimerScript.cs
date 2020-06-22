d4add880b500a949799d71768e432ae5e7d0a3cb
和田哲弥 <stardust0120000930@gmail.com>
2020-06-23 02:31:56 +0900

    ステートマシン用に調整

--- Assets/Project/Program/Timer/TimerScript.cs
+++ Assets/Project/Program/Timer/TimerScript.cs
@@ -7,9 +7,29 @@ public class TimerScript : MonoBehaviour
 {
     public bool playing;//試合中のフラグ
     bool end = false;//試合終了のフラグ
-    public float timer;
-    public GameObject finishMessage;//試合終了のメッセージを入れた物
-    Text timeText;
+    public float timer;//残り試合時間
+    Text timeText;//時間の表示
+
+    public bool isTimeUp()
+    {
+        return end;
+    }
+
+    public void setTimer(int min,int sec)
+    {
+        timer = min * 60 + sec;
+        end = false;
+    }
+
+    public void pause()
+    {
+        playing = false;
+    }
+
+    public void pauseEnd()
+    {
+        playing = true;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -22,21 +42,15 @@ public class TimerScript : MonoBehaviour
     {
         if (playing && !end)
         {
-            if (timer > 1)
+            if (timer > 0)
             {
-
                 timer -= Time.deltaTime;
-                timeText.text = ((int)(timer / 60)).ToString("0") + ":" + ((int)(timer % 60)).ToString("00");
-
+                timeText.text = ((int)(timer / 60)).ToString("0") + ":" + Mathf.CeilToInt(timer % 60).ToString("00");
             }
             else
             {
-                finishMessage.SetActive(true);//試合終了のメッセージを入れた物を表示
                 playing = false;
                 end = true;
-                //ここに多分処理を追加することになる
-                Time.timeScale = 0;
-
             }
         }
        
