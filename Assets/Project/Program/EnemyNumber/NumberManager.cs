45d955392f58602a43d841a86eff4f55b4eaa71f
Sugichan <sgnmssuioaai@gmail.com>
2020-07-09 21:19:16 +0900

    敵の残機表示

---
+++ Assets/Project/Program/EnemyNumber/NumberManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberManager : MonoBehaviour
+{
+    public EasyCPUManager cpuManager;
+    public Text enemyNumber;
+    public Text Point;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      enemyNumber.text="Team="+cpuManager.TeamMemberCount  + ", Opp=" + cpuManager.OpponentMemberCount;   
+    }
+}
