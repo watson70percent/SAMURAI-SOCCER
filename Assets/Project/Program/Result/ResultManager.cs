6172638ae108fc4107b091bda6331dcebd6a498a
Sugichan <sgnmssuioaai@gmail.com>
2020-08-06 21:47:39 +0900

    ResultSceneの設置

---
+++ Assets/Project/Program/Result/ResultManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ResultState
+{
+    Win,
+    TimeOver,
+    Violation
+}
+
+public class ResultManager : MonoBehaviour
+{
+
+    public ResultState resultState;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
