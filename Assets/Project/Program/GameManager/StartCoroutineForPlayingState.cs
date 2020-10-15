4d01d40b5850f13aab0d0ca2b401375df97735d4
DESKTOP-APK21IT\itoka229 <itoka229@yahoo.co.jp>
2020-10-15 23:31:42 +0900

    GameManagerTagの追加、アメリカシーンの追加、自作コルーチンの追加

---
+++ Assets/Project/Program/GameManager/StartCoroutineForPlayingState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GameManager))]
+public class StartCoroutineForPlayingState : MonoBehaviour
+{
+    public static List<IEnumerator> taskEnumerators = new List<IEnumerator>();
+    private GameManager _gameManager;
+
+    public void Start()
+    {
+        _gameManager = GetComponent<GameManager>();
+    }
+
+    public void Update()
+    {
+        if (_gameManager.CurrentGameState != GameState.Playing)
+        {
+            return;
+        }
+
+        List<int> idx = new List<int>();
+        for (int i = 0;i < taskEnumerators.Count;i++)
+        {
+            if (!taskEnumerators[i].MoveNext())
+            {
+                idx.Add(i);
+            }
+        }
+        for (int i = 0; i < idx.Count; i++)
+        {
+            taskEnumerators.RemoveAt(idx[idx.Count - 1 - i]);
+        }
+    }
+}
