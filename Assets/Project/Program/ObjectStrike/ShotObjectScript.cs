18a6b9a1f69e900eae70a5d4350161d1b77f3863
watson70percent <gozaru5364@gmail.com>
2020-12-03 12:38:06 +0900

    前から車が！機能実装

---
+++ Assets/Project/Program/ObjectStrike/ShotObjectScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShotObjectScript : MonoBehaviour
+{
+    private string ResultSceneName = "Result";
+    float velocity = 30;//速さ
+	//float size;
+    float movedLength;//動いた距離
+    float groundWidth=120;//グラウンドの幅
+    GameManager gameManager;
+
+    //   public ShotObjectScript()
+    //{
+
+    //}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //移動距離の初期化
+        movedLength = 0;
+        //gameManager取得
+        gameManager = this.gameObject.transform.root.GetComponent<StrikeObjectScript>().gameManager;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //一定スピードで動かす
+		gameObject.transform.position += transform.forward * Time.deltaTime * velocity;
+        movedLength+= Time.deltaTime * velocity;
+
+        //グラウンドを通り過ぎたら消す
+        if ((movedLength) > (groundWidth + 2))
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+	private void OnCollisionEnter(Collision collision)
+	{
+
+        //衝突が人間だったら吹っ飛ばす
+
+            Rigidbody rb = collision.transform.GetComponent<Rigidbody>();
+            Vector3 v3 = transform.forward * velocity * velocity;
+            v3.y += velocity * 5;
+            rb.AddForce(v3);
+        //衝突がプレイヤーだったらゲームオーバー
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.sceneLoaded += GameSceneLoaded;
+            gameManager.StateChangeSignal(GameState.Finish);
+            //リザルトへのシーン遷移
+            StartCoroutine(GoResult());
+        }
+
+
+    }
+
+    //スロー演出からのシーン遷移
+    IEnumerator GoResult()
+    {
+        yield return new WaitForSeconds(0.8f);
+        Time.timeScale = 1;
+        SceneManager.LoadScene(ResultSceneName);
+    }
+
+
+
+    //交通事故リザルト用の処理
+    void GameSceneLoaded(Scene next, LoadSceneMode mode)
+    {
+        ResultManager resultManager = GameObject.Find("ResultManager").GetComponent<ResultManager>();
+        resultManager.SetResult(Result.Lose, "交通事故");
+
+        SceneManager.sceneLoaded -= GameSceneLoaded;
+    }
+
+}
---
+++ Assets/Project/Program/ObjectStrike/StrikeObjectScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrikeObjectScript : MonoBehaviour
+{
+    public float shotInterval;//障害物生成間隔
+    float elapsedTime;//生成後経過した時間
+    public GameObject ShotObject;//生成するobject
+    float[] shotPosZ = {15.5f, 50, 84.3f };//グラウンドの道路の座標
+    public GameManager gameManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //時間が経ったらobject生成
+        if (elapsedTime>=shotInterval)
+        {
+            Vector3 pos = transform.position;
+            pos.z = shotPosZ[(int)Random.Range(0, 3)];
+            transform.position = pos;
+            elapsedTime = 0.0f;
+            GameObject realObject = Instantiate(ShotObject, this.transform.position, transform.rotation, this.transform);
+
+        }
+        //時間増やす
+        elapsedTime += Time.deltaTime;
+    }
+
+    
+
+}
