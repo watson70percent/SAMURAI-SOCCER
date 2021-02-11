f736f5d4a9558a7692a82845c35a965005b008a0
watson70percent <gozaru5364@gmail.com>
2021-02-11 23:08:04 +0900

    車機能完了

---
+++ Assets/Project/Program/ObjectStrike/AvoidCarScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvoidCarScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Ball" && other.tag != "Player")
+        {
+            //Debug.Log("colider.tag = "+other.tag);
+            Rigidbody rb = other.transform.GetComponent<Rigidbody>();
+            Vector3 v3 = Vector3.zero;
+            v3.y += 300;
+            rb.AddForce(v3);
+        }
+
+        //   if()
+    }
+}
