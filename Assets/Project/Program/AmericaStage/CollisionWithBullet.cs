3445e1d73e7897b142b74280a4ffe482af6b38ed
itoka229 <itoka229@yahoo.co.jp>
2021-02-18 21:44:35 +0900

    Swatステージの敵から弾が発射されて当たると死ぬまでの機構を作成

---
+++ Assets/Project/Program/AmericaStage/CollisionWithBullet.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 弾丸との衝突の処理
+/// </summary>
+public class CollisionWithBullet : MonoBehaviour
+{
+    private GameManager _gameManager;
+
+    private AudioSource _audioSource;//弾丸についてるAudioSource
+
+    public string ResultSceneName = "Result";//リザルトシーン名
+
+    void Start()
+    {
+        _gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        _audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //プレイヤ―との衝突処理
+        if (_gameManager.CurrentGameState != GameState.Finish)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                //死亡音の再生
+                _audioSource.Play();
+                //SceneManagerのイベントに圧死リザルト処理を追加
+                SceneManager.sceneLoaded += GameSceneLoaded;
+                _gameManager.StateChangeSignal(GameState.Finish);
+                Time.timeScale = 0.2f;
+                StartCoroutine(GoResult());
+            }
+        }
+    }
+
+    /// <summary>
+    /// 圧死リザルト用の処理
+    /// </summary>
+    /// <param name="next"></param>
+    /// <param name="mode"></param>
+    void GameSceneLoaded(Scene next, LoadSceneMode mode)
+    {
+        ResultManager resultManager = GameObject.Find("ResultManager").GetComponent<ResultManager>();
+        resultManager.SetResult(Result.Lose, "睡眠弾に撃たれた！");
+
+        SceneManager.sceneLoaded -= GameSceneLoaded;
+    }
+
+    /// <summary>
+    /// リザルトへ移動するためのコルーチン
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator GoResult()
+    {
+        yield return new WaitForSeconds(1f);
+        Time.timeScale = 1;
+        SceneManager.LoadScene(ResultSceneName);
+    }
+
+}
---
+++ Assets/Project/Program/AmericaStage/FireHypnoticBullets.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 敵選手に付けて弾を撃たせる
+/// </summary>
+public class FireHypnoticBullets : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject HypnoticBullets;//撃つ催眠弾
+
+    private float _fireDuration = 10.0f;//弾を撃つ間隔
+    private GameManager _gameManager;//Scene内のGameManagerクラス
+    private GameObject _Samurai;//侍のオブジェクト
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameManagerなかったらエラーなのでこの先は動かさない。
+        if (!GameObject.FindGameObjectWithTag("GameManager"))
+        {
+            Debug.LogError("GameManagerが存在しません");
+        }
+        _gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        _Samurai = GameObject.FindGameObjectWithTag("Player");
+
+        //弾を撃つ間隔を指定
+        _fireDuration = Random.Range(6.0f,10.0f);
+        StartCoroutine(FireBulletSycle());
+    }
+
+    /// <summary>
+    /// 一定時間ごとに弾を撃つ
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator FireBulletSycle()
+    {
+        yield return new WaitForSeconds(_fireDuration+10f);
+        while (_gameManager.CurrentGameState != GameState.Finish)
+        {
+            if (_gameManager.CurrentGameState == GameState.Playing)
+            {
+                //侍との相対座標
+                Vector3 firedirection = _Samurai.transform.position - gameObject.transform.position;
+                //敵選手が向く方向を計算し、反映
+                Quaternion firelotation = Quaternion.LookRotation(firedirection);
+                gameObject.transform.localRotation = firelotation;
+                //弾を生成し、発射
+                GameObject Bullet = Instantiate(HypnoticBullets);
+                Bullet.GetComponent<Rigidbody>().velocity = gameObject.transform.forward * 10;
+                Bullet.transform.position = gameObject.transform.position;
+            }
+            yield return new WaitForSeconds(_fireDuration);
+        }
+    }
+}
