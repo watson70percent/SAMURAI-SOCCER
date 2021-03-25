143dd86f965a823fa673a7f6aa0fb2f049d67616
itoka229 <itoka229@yahoo.co.jp>
2021-03-25 21:25:42 +0900

    下のコミットで上げ忘れたファイル

---
+++ Assets/Project/Program/StageSelect/MapStageData/BackWholeMap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(StageSelectCamera))]
+public class BackWholeMap : MonoBehaviour
+{
+    [SerializeField]
+    private float _cameraDistance = 1000f;//カメラと対象オブジェクトとの距離
+    [SerializeField]
+    private CinemachineVirtualCamera _WholeMapVirtualCamera;//全体世界地図用のvirtualCamera
+
+    private StageSelectCamera _selectCamera;
+
+    private void Start()
+    {
+        _WholeMapVirtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>().m_CameraDistance = _cameraDistance;
+        _selectCamera = GetComponent<StageSelectCamera>();
+    }
+
+    public void Back()
+    {
+        _selectCamera.ChangeCameraView(_WholeMapVirtualCamera);
+    }
+}
