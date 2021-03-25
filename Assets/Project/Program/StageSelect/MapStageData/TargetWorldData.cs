804e9b275544398fdfa84ab7860e8a28a4c0d827
itoka229 <itoka229@yahoo.co.jp>
2021-03-25 21:24:27 +0900

    StageのSave,Load,マップの拡大、縮小を行うスクリプトを作成

---
+++ Assets/Project/Program/StageSelect/MapStageData/TargetWorldData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class TargetWorldData : MonoBehaviour
+{
+    [SerializeField]
+    private float _cameraDistance = 5f;//カメラと対象オブジェクトとの距離
+    public float FocusStrength
+    {
+        get { return _cameraDistance; }
+        private set { _cameraDistance = value; }
+    }
+
+    private void Start()
+    {
+        GetComponentInChildren<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineFramingTransposer>().m_CameraDistance = _cameraDistance;
+    }
+}
