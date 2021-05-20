c017f9e4f9dbd91b2ceb56a81f564b53b0f8a498
kumaS-nu <kumas.kumachan@gmail.com>
2021-05-20 22:45:01 +0900

    スケーラー書いた。

---
+++ Assets/Project/Program/UIEffects/UISafeAreaSupport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISafeAreaSupport : MonoBehaviour
+{
+    public CanvasScaler scaler;
+
+    private void Awake()
+    {
+        if(Screen.safeArea != new Rect(0, 0, Screen.width, Screen.height))
+        {
+            var reference = new Vector2(scaler.referenceResolution.x * Screen.width / Screen.safeArea.width, scaler.referenceResolution.y * Screen.height / Screen.safeArea.height);
+            scaler.referenceResolution = reference;
+        }
+    }
+}
