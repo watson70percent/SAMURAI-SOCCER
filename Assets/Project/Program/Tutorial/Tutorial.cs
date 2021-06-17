2d2d148722ed7c389ede7bb09158e9e63a915536
itoka229 <itoka229@yahoo.co.jp>
2021-06-18 00:04:36 +0900

    Tutorialのアニメーションを追加

--- Assets/Project/Program/Tutorial/Tutorial.cs
+++ Assets/Project/Program/Tutorial/Tutorial.cs
@@ -8,14 +8,15 @@ using Cinemachine;
 [RequireComponent(typeof(GameManager))]
 public class Tutorial : MonoBehaviour
 {
-
     private GameManager gameManager;
     public GameObject samurai;
     public GameObject ball;
     public GameObject firstYellowCard;
     public GameObject enemy;
     public Text tutorialText;
-    public Animator animator;
+    public Text enemyNumber;
+    public Animator textAnimator;
+    public Animator arrowAnimator;
     public CinemachineVirtualCamera spotCamera; //何か焦点を当てるためのカメラ
     public CinemachineVirtualCamera samuraiCamera; //侍を追尾するためのカメラ
 
@@ -33,27 +34,25 @@ public class Tutorial : MonoBehaviour
         yield return new WaitForSeconds(5f);
         tutorialText.text = "ここではこの世界で戦うちゅーとりあるを行う";
         yield return new WaitForSeconds(3f); 
-        tutorialText.text = "貴殿は残された日本の希望だ";
+        tutorialText.text = "サムライは残された日本の希望だ";
         yield return new WaitForSeconds(3f);
         Vector3 destination = new Vector3(10f, 3f, 30f);
         var enemyPrefab = Instantiate(enemy,destination,Quaternion.identity);
-        /* TODO : UIを突っ込む*/
         //カメラを生成された選手に向ける
         spotCamera.Follow = enemyPrefab.transform;
         spotCamera.LookAt = enemyPrefab.transform;
         spotCamera.Priority = 11;
         yield return new WaitForSeconds(3f);
-        tutorialText.text = "まずはそこまで行こう";
+        tutorialText.text = "まずはここまで行こう";
         yield return new WaitForSeconds(3f);
         //カメラをもとに戻す
         spotCamera.Priority = 9;
         samuraiCamera.Priority = 11;
         yield return new WaitForSeconds(3f);
-        animator.SetTrigger("SlideText");
+        textAnimator.SetTrigger("SlideText");
         yield return new WaitForSeconds(2f);
         //テキストを非表示に
         tutorialText.text = "";
-        animator.SetTrigger("ReturnText");
         gameManager.StateChangeSignal(GameState.Playing);
         //敵に一定距離近づくまで待機
         while ((samurai.transform.position-destination).sqrMagnitude > 100)
@@ -62,33 +61,37 @@ public class Tutorial : MonoBehaviour
         }
         gameManager.StateChangeSignal(GameState.Standby);
         //テキスト表示2
-        tutorialText.text = "貴殿が行うのはただ斬ることのみ";
+        textAnimator.SetTrigger("ReturnText");
+        tutorialText.text = "サムライが行うのはただ斬ることのみ";
         yield return new WaitForSeconds(3f);
         tutorialText.text = "試しにこいつをひとおもいに斬れ";
         yield return new WaitForSeconds(3f);
-        animator.SetTrigger("SlideText");
+        textAnimator.SetTrigger("SlideText");
         yield return new WaitForSeconds(2f);
         //テキストを非表示に
         tutorialText.text = "";
-        animator.SetTrigger("ReturnText");
         gameManager.StateChangeSignal(GameState.Playing);
         //敵を切り倒して行って距離移動するまで待機
         while ((enemyPrefab.transform.position - destination).sqrMagnitude < 400)
         {
             yield return null;
         }
+        gameManager.StateChangeSignal(GameState.Pause);
+        enemyNumber.text = 1.ToString();
         //テキスト表示2
+        textAnimator.SetTrigger("ReturnText");
         tutorialText.text = "よし、それでいい";
         yield return new WaitForSeconds(3f);
         //ボールの位置を移動する
         destination = new Vector3(50f, 3f, 60f);
         ball.transform.position = destination;
         enemyPrefab = Instantiate(enemy, destination, Quaternion.identity);
+        yield return new WaitForSeconds(1f);
         //カメラを生成された選手に向ける
-        samuraiCamera.Priority = 9;
         spotCamera.Follow = enemyPrefab.transform;
         spotCamera.LookAt = enemyPrefab.transform;
         spotCamera.Priority = 11;
+        samuraiCamera.Priority = 9;
         yield return new WaitForSeconds(3f);
         tutorialText.text = "次はこいつを斬れ";
         yield return new WaitForSeconds(3f);
@@ -96,36 +99,39 @@ public class Tutorial : MonoBehaviour
         spotCamera.Priority = 9;
         samuraiCamera.Priority = 11;
         yield return new WaitForSeconds(3f);
-        animator.SetTrigger("SlideText");
+        textAnimator.SetTrigger("SlideText");
         yield return new WaitForSeconds(2f);
         //テキストを非表示に
         tutorialText.text = "";
-        animator.SetTrigger("ReturnText");
         gameManager.StateChangeSignal(GameState.Playing);
         //イエローカードが出るまで待機
         while (!firstYellowCard.activeSelf)
         {
-            Debug.Log(firstYellowCard.activeSelf);
             yield return null;
-            break;
         }
+        gameManager.StateChangeSignal(GameState.Pause);
         //テキスト表示3
-        tutorialText.text = "しまった審判に見られてしまった";
+        textAnimator.SetTrigger("ReturnText");
+        tutorialText.text = "しまった！れふぇりーに見られてしまった";
         yield return new WaitForSeconds(3f);
         tutorialText.text = "もう一度見つかると退場になってしまう、気をつけろ";
         yield return new WaitForSeconds(3f);
         tutorialText.text = "最後に確認する";
         yield return new WaitForSeconds(3f);
+        arrowAnimator.gameObject.SetActive(true);
         tutorialText.text = "これは残りの敵数だ";
         yield return new WaitForSeconds(3f);
-        tutorialText.text = "0になれば貴方の勝ちだ";
+        tutorialText.text = "0になれば日本の勝利だ";
         yield return new WaitForSeconds(3f);
+        arrowAnimator.SetTrigger("MoveArrow");
+        yield return new WaitForSeconds(1f);
         tutorialText.text = "これは残り時間だ";
         yield return new WaitForSeconds(2f);
         tutorialText.text = "無くなるまでにクリアせよ";
         yield return new WaitForSeconds(3f);
-        tutorialText.text = "それではちゅーとりあるを終了する";
+        tutorialText.text = "それではちゅーとりあるを終わる";
         yield return new WaitForSeconds(3f);
+        arrowAnimator.gameObject.SetActive(false);
         tutorialText.text = "必ず日本に勝利を持ち帰れ";
         yield return new WaitForSeconds(5f);
         SceneManager.LoadScene("StageSelect");
