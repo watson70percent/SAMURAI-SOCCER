9f9ef6e542cb62433007310df39f91d3374998b5
Sugichan <sgnmssuioaai@gmail.com>
2022-06-09 21:21:05 +0900

    Dataholderの修正

--- Assets/Scripts/StageContents/China/Panda.cs
+++ Assets/Scripts/StageContents/China/Panda.cs
@@ -1,84 +1,90 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using SamuraiSoccer.Event;
+using UniRx;
+using SamuraiSoccer;
 
-public class Panda : MonoBehaviour
+namespace SamuraiSoccer.StageContents.China
 {
-    private float m_expandAmount = 400.0f;
-    [SerializeField] private float speed;
-    bool hit;
-    GameManager gameManager;
-    [SerializeField] GameState state = GameState.Reset;
-    Animator anim;
-    [SerializeField] SkinnedMeshRenderer skin;
-    public GameObject gameOverPanel;
-    public AudioClip hitSound;
-    public GameObject blood;
-    GameObject player;
-
-    void SwitchState(StateChangedArg a)
+    public class Panda : MonoBehaviour
     {
-        state = a.gameState;
-    }
 
-    private void Start()
-    {
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        gameManager.StateChange += SwitchState;
-        state = gameManager.CurrentGameState;
-        anim = GetComponent<Animator>();
-
-        player = GameObject.FindGameObjectWithTag("Player");
-        
-    }
-
-    private void Update()
-    {
-        switch (state)
+        enum State
         {
-            case GameState.Playing:
-                var pos = transform.position;
-                pos.y -= speed * Time.deltaTime;
-                transform.position = pos;
-                if (pos.y < -50) { Destroy(gameObject); }
-                anim.speed = 1;
-                break;
-            case GameState.Standby:
-                Destroy(gameObject);
-                break;
-            default: anim.speed = 0; break;
+            Moving, Stop
+        }
+
+        private float m_expandAmount = 400.0f;
+        [SerializeField] private float speed;
+        bool hit;
+
+        State state = State.Moving;
+        Animator anim;
+        [SerializeField] SkinnedMeshRenderer skin;
+        public GameObject gameOverPanel;
+        public AudioClip hitSound;
+        public GameObject blood;
+        GameObject player;
+
+
+
+        private void Start()
+        {
+            InGameEvent.Pause.Subscribe(isPause => { state = isPause ? State.Stop : State.Moving; });
+            InGameEvent.Standby.Subscribe(x => { Destroy(gameObject); });
+
+            anim = GetComponent<Animator>();
+
+            player = GameObject.FindGameObjectWithTag("Player");
+
+        }
+
+        private void Update()
+        {
+            switch (state)
+            {
+                case State.Moving:
+                    var pos = transform.position;
+                    pos.y -= speed * Time.deltaTime;
+                    transform.position = pos;
+                    if (pos.y < -50) { Destroy(gameObject); }
+                    anim.speed = 1;
+                    break;
+                case State.Stop:
+                    default: anim.speed = 0; break;
+            }
+
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player" && !hit)
+            {
+                hit = true;
+                Invoke("GameOver", 0.2f);
+            }
         }
 
 
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player" && !hit)
+        public void GameOver()
         {
-            hit = true;
-            Invoke("GameOver", 0.2f);
+
+            InGameEvent.FinishOnNext();
+            SoundBoxUtil.SetSoundBox(transform.position, hitSound);
+
+            Instantiate(blood, player.transform.position + Vector3.up * 0.1f, Quaternion.identity);
+            Instantiate(gameOverPanel);
+
         }
-    }
 
+        void GameSceneLoaded(Scene next, LoadSceneMode mode)
+        {
+            ResultManager resultManager = GameObject.Find("ResultManager").GetComponent<ResultManager>();
+            resultManager.SetResult(Result.Lose, "パンダに潰されてしもた!");
 
-    public void GameOver()
-    {
-
-        SceneManager.sceneLoaded += GameSceneLoaded;
-        gameManager.StateChangeSignal(GameState.Finish);
-        SoundBoxUtil.SetSoundBox(transform.position, hitSound);
-
-        Instantiate(blood, player.transform.position + Vector3.up * 0.1f, Quaternion.identity);
-        Instantiate(gameOverPanel);
+            SceneManager.sceneLoaded -= GameSceneLoaded;
+        }
 
     }
-
-    void GameSceneLoaded(Scene next, LoadSceneMode mode)
-    {
-        ResultManager resultManager = GameObject.Find("ResultManager").GetComponent<ResultManager>();
-        resultManager.SetResult(Result.Lose, "パンダに潰されてしもた!");
-
-        SceneManager.sceneLoaded -= GameSceneLoaded;
-    }
-
 }
