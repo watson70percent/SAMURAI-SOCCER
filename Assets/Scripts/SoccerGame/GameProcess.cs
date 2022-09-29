eeaa92547d5168a798351771bdfdd0d6b5c7f053
itoka229 <itoka229mori@gmail.com>
2022-09-29 23:04:44 +0900

    ゴールとフィニッシュの挙動をGameProcessから切り離した

--- Assets/Scripts/SoccerGame/GameProcess.cs
+++ Assets/Scripts/SoccerGame/GameProcess.cs
@@ -3,24 +3,11 @@ using UniRx;
 using Cysharp.Threading.Tasks;
 using SamuraiSoccer.Event;
 using SamuraiSoccer;
-using UnityEngine.SceneManagement;
 
 namespace SamuraiSoccer.Event
 {
     public class GameProcess : MonoBehaviour
     {
-        [SerializeField]
-        private string m_resultSceneName = "Result";
-
-        [SerializeField]
-        private int m_goalDelayMiliSec = 5000;
-
-        [SerializeField]
-        private int m_finishDelayMiliSec = 1000;
-
-        [SerializeField]
-        private Animator m_goalFadeAnimator;
-
         [SerializeField]
         [Tooltip("ほら貝のSE")]
         private int m_trumpetShellSENumber;
@@ -28,10 +15,6 @@ namespace SamuraiSoccer.Event
         [SerializeField]
         [Tooltip("ホイッスル開始音")]
         private int m_whistleSENumber;
-
-        [SerializeField]
-        [Tooltip("ホイッスル音(短い)")]
-        private int m_shortWhistleSENumber;
 
         [SerializeField]
         [Tooltip("ゲームのBGM")]
@@ -45,19 +28,9 @@ namespace SamuraiSoccer.Event
                 await ResetContents();
             });
 
-            InGameEvent.Standby.Subscribe(async _ =>
+            InGameEvent.Standby.First().Subscribe(async _ =>
             {
-                await StandbyContents();
-            });
-
-            InGameEvent.Goal.Subscribe(async _ =>
-            {
-                await GoalContents();
-            });
-
-            InGameEvent.Finish.Subscribe(async _ =>
-            {
-                await FinishContents();
+                await FirstStandbyContents();
             });
 
             InGameEvent.ResetOnNext();
@@ -67,35 +40,17 @@ namespace SamuraiSoccer.Event
         {
             await UniTask.Delay(1000);
             SoundMaster.Instance.PlaySE(m_trumpetShellSENumber); //ほら貝のSEを流す
-            await UniTask.Delay(6000);
-            SoundMaster.Instance.PlayBGM(m_gameBGM); //ゲームのBGMを流す
             InGameEvent.StandbyOnNext();
         }
 
-        private async UniTask StandbyContents()
+        private async UniTask FirstStandbyContents()
         {
-            await UniTask.Delay(500);
+            await UniTask.Delay(6000); 
             SoundMaster.Instance.PlaySE(m_whistleSENumber); //ホイッスルのSEを流す
+            await UniTask.Delay(500);
+            SoundMaster.Instance.PlayBGM(m_gameBGM); //ゲームのBGMを流す
             InGameEvent.PlayOnNext();
         }
-
-        private async UniTask GoalContents()
-        {
-            SoundMaster.Instance.PlaySE(m_whistleSENumber); //ホイッスルのSEを流す
-            await UniTask.Delay(m_goalDelayMiliSec);
-            m_goalFadeAnimator.SetTrigger("Fade");
-            await UniTask.Delay(500);
-            InGameEvent.StandbyOnNext();
-        }
-
-        public async UniTask FinishContents()
-        {
-            //シーンの移動処理
-            await UniTask.Delay(m_finishDelayMiliSec);
-            Time.timeScale = 1;
-            SceneManager.LoadScene(m_resultSceneName);
-        }
-
     }
 }
 
