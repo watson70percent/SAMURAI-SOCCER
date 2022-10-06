6b170375049a52d9da905f960cc4307bc8c89a76
itoka229 <itoka229mori@gmail.com>
2022-10-06 22:47:27 +0900

    試合終了後即再選したときに試合開始前にBGMがなってしまう問題を解消

--- Assets/Scripts/StageContents/Result/ResultBGM.cs
+++ Assets/Scripts/StageContents/Result/ResultBGM.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UniRx;
 using Cysharp.Threading.Tasks;
+using System.Threading;
 
 namespace SamuraiSoccer.StageContents.Result
 {
@@ -19,29 +21,46 @@ namespace SamuraiSoccer.StageContents.Result
         [SerializeField]
         private int m_loseBGMNum; //敗北時BGM番号
 
+        private CancellationTokenSource m_cancellationTokenSource = new CancellationTokenSource();
+
         // Start is called before the first frame update
         async void Start()
         {
+            SceneManager.sceneLoaded += SceneLoaded;
             if (m_rm.ResultState == GameResult.Win)
             {
-                await PlayWinBGM();
+                await PlayWinBGM(m_cancellationTokenSource.Token);
             }
             else
             {
-                await PlayLoseBGM();
+                await PlayLoseBGM(m_cancellationTokenSource.Token);
             }
         }
 
-        private async UniTask PlayWinBGM()
+        private async UniTask PlayWinBGM(CancellationToken cancellationToken)
         {
             await SoundMaster.Instance.PlaySE(m_winSENum);
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return;
+            }
             SoundMaster.Instance.PlayBGM(m_winBGMNum);
         }
 
-        private async UniTask PlayLoseBGM()
+        private async UniTask PlayLoseBGM(CancellationToken cancellationToken)
         {
             await SoundMaster.Instance.PlaySE(m_loseSENum);
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return;
+            }
             SoundMaster.Instance.PlayBGM(m_loseBGMNum);
+        }
+
+        void SceneLoaded(Scene nextScene, LoadSceneMode mode)
+        {
+            m_cancellationTokenSource.Cancel();
+            SceneManager.sceneLoaded -= SceneLoaded;
         }
     }
 }
