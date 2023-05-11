f1dbcc9287d9ce4a867054c0cfd768a467d1935b
itoka229 <itoka229mori@gmail.com>
2023-05-11 23:11:23 +0900

    太った選手とチュートリアルの選手の斬られたときの処理を実装

---
+++ Assets/Scripts/SoccerGame/TutorialEnemySlashed.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SamuraiSoccer.SoccerGame
+{
+    public class TutorialEnemySlashed : MonoBehaviour, ISlashed
+    {
+        [SerializeField]
+        private Rigidbody m_rigidbody;
+
+        public void Slashed(Vector3 dir)
+        {
+            m_rigidbody.AddForce(dir * 1000, ForceMode.Impulse);
+            _ = SoundMaster.Instance.PlaySE(3); // Ža‚ç‚ê‚½SE
+        }
+    }
+}
