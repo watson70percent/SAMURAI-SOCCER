dfad7c01be7f7d1a74db7e73094ddde0f086c3c0
kumaS-nu <kumachan0220ios@gmail.com>
2023-11-05 17:54:11 +0900

    最適化

--- Assets/Scripts/StageContents/BattleDome/Indicator.cs
+++ Assets/Scripts/StageContents/BattleDome/Indicator.cs
@@ -1,177 +1,92 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-using System.IO;
 using System;
-using Newtonsoft.Json;
+using Cysharp.Threading.Tasks;
 
 namespace SamuraiSoccer.StageContents.BattleDome
 {
     public class Indicator : MonoBehaviour
     {
-        public GameObject indicator;
-        public AudioSource bgm;
-        public Vector3 center;
-        public int h;
-        public float width;
-        public float height;
-        public int w_div;
-        public int h_div;
-        public float quarter;
-        public float floor;
-        public float low;
-        public float high;
+        public GameObject m_indicatorPlane;
+        public AudioSource m_bgm;
 
-        private GameObject[][] objs;
-        private BGMData bgmData;
-        private DateTime start_time;
-        private List<float> level_table;
-        private List<List<int>> f_table;
+        private Material[] m_objs;
+        private BSA bgmData;
+        private DateTime m_start_time;
+        private double m_levelNormalizer;
+
+        private readonly Vector3 m_center = new Vector3(61, 3.65f, 49);
+        private readonly float m_width = 2.0f;
+        private readonly string m_fileName = "samurai-full-flavor.bsa";
 
         // Start is called before the first frame update
-        void Start()
+        async UniTask Start()
         {
             var time = DateTime.Now;
-            var textData = File.ReadAllText("D:\\Program\\VSCode\\fft\\侍サッカー2_ver_full_flavor.json");
+            bgmData = new BSA();
+            await bgmData.Load(m_fileName);
+            m_levelNormalizer = bgmData.LevelUnit;
             var dt = DateTime.Now - time;
             time = DateTime.Now;
             Debug.Log("t1 is:" + dt.TotalMilliseconds);
-            bgmData = JsonConvert.DeserializeObject<BGMData>(textData);
+
+            var w_start = m_center.z - m_width * bgmData.FCount / 2 + m_width / 2;
+            var grid = Enumerable.Range(0, bgmData.FCount).Select(i => new Vector3(m_center.x, m_center.y, w_start + i * m_width)).Reverse().ToArray();
+
             dt = DateTime.Now - time;
             time = DateTime.Now;
             Debug.Log("t2 is:" + dt.TotalMilliseconds);
-            var i_width = width / w_div;
-            var i_height = height / h_div;
-            var z_scale = i_width;
-            var xy_scale = i_height / 0.2f;
-            var scale = new Vector3(xy_scale, xy_scale, z_scale);
-            var w_start = center.z - width / 2 + i_width / 2;
-            var h_start = h switch
+
+            m_objs = grid.Select(g =>
             {
-                0 => center.x - height / 2 + i_height / 2,
-                1 => center.y - height / 2 + i_height / 2,
-                _ => throw new System.Exception("invalid h"),
-            };
-            var w_grid = Enumerable.Range(0, w_div).Select(i => w_start + i * i_width).Reverse().ToArray();
-            var h_grid = Enumerable.Range(0, h_div).Select(i => h_start + i * i_height).ToArray();
-            var grid = w_grid.Select(w_p =>
-            {
-                var r = h_grid.Select(h_p =>
-                {
-                    var r_ = h switch
-                    {
-                        0 => new Vector3(h_p, center.y, w_p),
-                        1 => new Vector3(center.x, h_p, w_p),
-                        _ => throw new System.Exception("invalid h"),
-                    };
-                    return r_;
-                }).ToArray();
-                return r;
+                var o = Instantiate<GameObject>(m_indicatorPlane, g, Quaternion.identity);
+                var m = o.GetComponentInChildren<Renderer>().material;
+                m.SetFloat("_Level", 0.0f);
+                return m;
             }).ToArray();
 
             dt = DateTime.Now - time;
             time = DateTime.Now;
             Debug.Log("t3 is:" + dt.TotalMilliseconds);
 
-            objs = grid.Select(gr =>
-            {
-                var c = gr.Select(g =>
-                {
-                    var o = Instantiate<GameObject>(indicator, g, Quaternion.identity);
-                    o.transform.localScale = scale;
-                    o.SetActive(false);
-                    return o;
-                }).ToArray();
-                return c;
-            }).ToArray();
-
-            dt = DateTime.Now - time;
-            time = DateTime.Now;
-            Debug.Log("t4 is:" + dt.TotalMilliseconds);
-
-            var tqn = h_div * 3 / 4;
-            var qn = h_div - h_div * 3 / 4;
-
-            level_table = Enumerable.Range(0, qn).Select(i => (float)i / qn * quarter + (float)(qn - i) / qn * floor).ToList();
-            level_table.AddRange(Enumerable.Range(0, tqn).Select(i => (tqn - i + 0.5f) / tqn * quarter));
-
-            var low_ln = Mathf.Log10(low);
-            var high_ln = Mathf.Log10(high);
-            var f_span_ln = (high_ln - low_ln) / (w_div - 1);
-            var f_th = Enumerable.Range(0, w_div).Select(i => Mathf.Pow(10, i * f_span_ln + low_ln)).ToList();
-            f_th.Add(44100);
-
-            f_table = new List<List<int>>();
-
-            {
-                int i = 0;
-                f_table = f_th.Select(th =>
-                {
-                    var idxes = new List<int>();
-                    while (i < bgmData.f_table.Count && bgmData.f_table[i] < th)
-                    {
-                        idxes.Add(i);
-                        i++;
-                    }
-                    return idxes;
-                }).ToList();
-            }
-
-            dt = DateTime.Now - time;
-            time = DateTime.Now;
-            Debug.Log("t5 is:" + dt.TotalMilliseconds);
-
-            start_time = DateTime.Now.AddSeconds(5.0);
+            m_start_time = DateTime.Now.AddSeconds(5.0);
         }
 
         private void Update()
         {
-            var t = DateTime.Now - start_time;
+            var t = DateTime.Now - m_start_time;
 
             if (t.TotalMilliseconds < 0)
             {
                 return;
             }
 
-            if (bgm.time >= bgm.clip.length)
+            if (m_bgm.time >= m_bgm.clip.length)
             {
-                foreach (var o in objs)
+                foreach (var m in m_objs)
                 {
-                    foreach (var o_ in o)
-                    {
-                        o_.SetActive(false);
-                    }
+                    m.SetFloat("_Level", 0.0f);
                 }
                 return;
             }
 
-            if (!bgm.isPlaying)
+            if (!m_bgm.isPlaying)
             {
-                bgm.Play();
+                m_bgm.Play();
             }
 
-            var t_idx = Mathf.FloorToInt(bgm.time / bgmData.time_span);
+            var t_idx = Mathf.FloorToInt(m_bgm.time / bgmData.StrideTime);
 
-            if (t_idx >= bgmData.data.Count)
+            if (t_idx >= bgmData.SampleCount)
             {
                 return;
             }
 
-            foreach (var (f_idxes, obj) in f_table.Zip(objs, (i, o) => (i, o)))
+            var levels = bgmData.LevelData(t_idx);
+
+            for (int i = 0; i < bgmData.FCount; i++)
             {
-                var avg = f_idxes.Select(i => bgmData.data[t_idx][i]).Average();
-                foreach (var (th, o) in level_table.Zip(obj, (l, o) => (l, o)))
-                {
-                    if (avg > th)
-                    {
-                        o.SetActive(true);
-                    }
-                    else
-                    {
-                        o.SetActive(false);
-                    }
-                }
+                m_objs[i].SetFloat("_Level", (float)(levels[i] * m_levelNormalizer));
             }
         }
     }
---
+++ Assets/Visual/Shaders/Indicator_plate.shader
@@ -0,0 +1,56 @@
+Shader"Custom/Indicator_plate"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+        _MainTex ("Texture", 2D) = "white" {}
+        _Level ("Level", Range(0,1)) = 0.0
+
+        [HDR]
+        _EmissionColor("Emission", color) = (0, 0, 0, 0)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Transparent" "Queue"="Transparent" }
+
+        CGPROGRAM
+        #pragma surface surf Standard alpha noshadow nofog noshadowmask finalcolor:final
+
+        #include "UnityCG.cginc"
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+        sampler2D _MainTex;
+            
+        fixed4 _Color;
+        fixed4 _EmissionColor;
+        float _Level;
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            float visible;
+            float4 col;
+            col = tex2D(_MainTex, IN.uv_MainTex) * _Color;
+            visible = step(IN.uv_MainTex.y, _Level);
+            visible = min(visible, col.a);
+            o.Albedo = col.rgb;
+            o.Emission = _EmissionColor.rgb * visible;
+            o.Metallic = 0.0;
+            o.Smoothness = 1.0;
+            o.Alpha = visible;
+        }
+
+        void final(Input IN, SurfaceOutputStandard o, inout fixed4 color)
+        {
+            float visible;
+            visible = step(_Color.a * 0.5, o.Alpha);
+            color = color * visible;
+        }
+
+        ENDCG
+    }
+    FallBack OFF
+}
