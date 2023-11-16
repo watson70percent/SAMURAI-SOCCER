bc59cb903a85dba15a409dececd47d82933039be
watson70percent <stardust0120000930@gmail.com>
2023-11-16 21:48:43 +0900

    エンディング用の操作を実装

--- Assets/Scripts/SoccerGame/EndingAim.cs
+++ Assets/Scripts/SoccerGame/EndingAim.cs
@@ -28,6 +28,14 @@ namespace SamuraiSoccer.SoccerGame
         public void MoveAim(Vector3 dir)
         {
             transform.position += dir;
+            Vector3 pos = transform.position;
+
+            if (pos.x > limitXMax.transform.position.x) pos.x = limitXMax.transform.position.x;
+            if (pos.y > limitYMax.transform.position.y) pos.y = limitYMax.transform.position.y;
+            if (pos.x < limitXMin.transform.position.x) pos.x = limitXMin.transform.position.x;
+            if (pos.y < limitYMin.transform.position.y) pos.y = limitYMin.transform.position.y;
+            transform.position = pos;
+
             slashParent.transform.LookAt(transform);
 
             // オブジェクトのワールド座標
