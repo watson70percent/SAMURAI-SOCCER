98803165c7d47b8b416f045ac7a3ac2a7e359573
itoka229 <itoka229mori@gmail.com>
2023-11-23 21:52:49 +0900

    会話を追加

---
+++ Assets/Scripts/StageContents/StageSelect/LastStageInvitation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SamuraiSoccer.StageContents.StageSelect
+{
+    public class LastStageInvitation : MonoBehaviour
+    {
+        [SerializeField]
+        private int m_lastStageNum;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+        
+        }
+    }
+}
