048f0be2eb2d6c181df5e385814f6b0a1c6842df
watson70percent <stardust0120000930@gmail.com>
2023-11-23 22:30:08 +0900

    とりあえずのエンディング要素を追加

--- Assets/Scripts/SoccerGame/EndingManagerScript.cs
+++ Assets/Scripts/SoccerGame/EndingManagerScript.cs
@@ -7,18 +7,42 @@ namespace SamuraiSoccer.SoccerGame
     public class EndingManagerScript : MonoBehaviour
     {
         [SerializeField]
-        
+        Material[] StaffList;
+        [SerializeField]
+        GameObject test;
+        [SerializeField]
+        float Interval;
+        [SerializeField]
+        float velocity;
+
+        GameObject staff;
+        Rigidbody rb;
+        Material material;
+        float elapsedTime;
+        int staffIndex=0;
+        bool is_staffEnd;
 
         // Start is called before the first frame update
         void Start()
         {
-        
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime > Interval && !is_staffEnd)
+            {
+                elapsedTime = 0;
+                staff = Instantiate(test);
+                staff.GetComponent<Rigidbody>().velocity = Vector3.down * velocity;
+                staff.GetComponent<MeshRenderer>().material = StaffList[staffIndex];
+                staffIndex++;
+            }
+            if(staffIndex >= StaffList.Length)
+            {
+                is_staffEnd = true;
+            }
         }
     }
 }
