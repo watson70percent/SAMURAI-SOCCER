3ea76054e70cabbadef5294e1183710bdd49f655
watson70percent <stardust0120000930@gmail.com>
2023-11-23 21:40:18 +0900

    EndingManagerの作成

---
+++ Assets/Scripts/SoccerGame/EndingManagerScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SamuraiSoccer.SoccerGame
+{
+    public class EndingManagerScript : MonoBehaviour
+    {
+        [SerializeField]
+        
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
