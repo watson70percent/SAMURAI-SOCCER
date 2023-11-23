5a74c631d60a203c2c7d5a4f6e924aab696121d2
itoka229 <itoka229mori@gmail.com>
2023-11-23 22:54:35 +0900

    ラストステージの試合開始までの流れ実装

--- Assets/Scripts/StageContents/StageSelect/LastStageInvitation.cs
+++ Assets/Scripts/StageContents/StageSelect/LastStageInvitation.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using SamuraiSoccer.StageContents.Conversation;
 
 namespace SamuraiSoccer.StageContents.StageSelect
 {
     public class LastStageInvitation : MonoBehaviour
     {
         [SerializeField]
-        private int m_lastStageNum;
+        private int m_lastStageSaveNumber;
+
+        [SerializeField]
+        private int m_lastStageConversationNumber;
+
+        [SerializeField]
+        private ConversationManager m_conversationManager;
 
         // Start is called before the first frame update
         private void Start()
         {
-        
+            InFileTransmitClient<SaveData> fileTransitClient = new InFileTransmitClient<SaveData>();
+            if (fileTransitClient.TryGet(StorageKey.KEY_STAGENUMBER, out var save))
+            {
+                if (save.m_stageData == m_lastStageSaveNumber)
+                {
+                    _ = m_conversationManager.PlayConversation(m_lastStageConversationNumber);
+                }
+            }
         }
     }
 }
