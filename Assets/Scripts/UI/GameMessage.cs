1c5a7d75f293a3503216ed5d9ca363241f9884a4
itoka229 <itoka229mori@gmail.com>
2024-03-21 22:52:02 +0900

    GoalとPenaltyの表記で表示される背景の色を自由に変更できるよう修正

--- Assets/Scripts/UI/GameMessage.cs
+++ Assets/Scripts/UI/GameMessage.cs
@@ -1,21 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UniRx;
 using TMPro;
 using SamuraiSoccer.Event;
+using UnityEngine.UI;
 
 public class GameMessage : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI m_text;
 
+    [SerializeField]
+    private Image m_backgroundImage;
+
+    [SerializeField]
+    private Color m_goalColor;
+
+    [SerializeField]
+    private Color m_penaltyColor;
+
     private float m_time = 100;
 
     private void Start()
     {
-        InGameEvent.Goal.Where(t => t == GoalEventType.NormalTeammateGoal || t == GoalEventType.NormalOpponentGoal).Subscribe(_ => TextContent = "Goal!").AddTo(this);
-        InGameEvent.Penalty.Subscribe(_ => TextContent = "反則!").AddTo(this);
+        InGameEvent.Goal.Where(t => t == GoalEventType.NormalTeammateGoal || t == GoalEventType.NormalOpponentGoal).Subscribe(_ => 
+        {
+            m_backgroundImage.color = m_goalColor;
+            TextContent = "Goal!";           
+        }).AddTo(this);
+        InGameEvent.Penalty.Subscribe(_ =>
+        {
+            m_backgroundImage.color = m_penaltyColor;
+            TextContent = "反則!";            
+        }).AddTo(this);
     }
     public string TextContent 
     { 
@@ -29,6 +45,7 @@ public class GameMessage : MonoBehaviour
     private void Update()
     {
         m_text.color = new Color(0, 0, 0, (4 - m_time) / 2);
+        m_backgroundImage.color = new Color(m_backgroundImage.color.r, m_backgroundImage.color.g, m_backgroundImage.color.b, (4 - m_time) / 2);
         m_time += m_time > 100 ? 0 : Time.deltaTime;
     }
 }
